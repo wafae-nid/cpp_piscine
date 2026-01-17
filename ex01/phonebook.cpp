4fc91b65c911f600244314ee4c1f659d15f99b56
Wafae Nid hsain <wafaenido8@gmail.com>
2026-01-17 16:04:07 +0100

    added getters

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: wnid-hsa <wnid-hsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:22:02 by wnid-hsa          #+#    #+#             */
-/*   Updated: 2026/01/17 13:57:50 by wnid-hsa         ###   ########.fr       */
+/*   Updated: 2026/01/17 16:02:10 by wnid-hsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,32 @@ class contact
         {
             first_name = value;
         }
+        void get_first_name()
+        {
+            return(first_name);
+        }
         void set_last_name(std::string value)
         {
             last_name = value;
+        }
+        void get_last_name()
+        {
+            return(last_name);
         }
         void set_phone_nbr(std::string value)
         {
             phone_number = value;
         }
+        void get_phone_nbr()
+        {
+            return(phone_number);
+        }
         void set_dark_sec(std::string value)
         {
             darkest_sec = value;
+        }
+        void get_dark_sec()
+        {
+            return(darkest_sec);
         }
 };
