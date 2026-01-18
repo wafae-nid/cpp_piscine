bfa9d5b6460cdd19078b421dc71e6ff8ee654c70
Wafae Nid hsain <wafaenido8@gmail.com>
2026-01-18 14:05:20 +0100

    working contact class + better inplementation

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,67 +6,42 @@
 /*   By: wnid-hsa <wnid-hsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:22:02 by wnid-hsa          #+#    #+#             */
-/*   Updated: 2026/01/17 17:50:26 by wnid-hsa         ###   ########.fr       */
+/*   Updated: 2026/01/18 14:04:18 by wnid-hsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-void Contact::set_first_name(const std::string &value)
+void Contact::set_contact()
 {
-    first_name = value;
+    for (int i = 0; i < 4; i++) {
+        if(!std::getline(std::cin, contact[i]))
+        {
+            std::cout<< "you successfully exited the program \n";
+            exit(0);
+        }
+        if(contact[i].empty())
+        {
+            while(contact[i].empty())
+            {
+                std::cout<< "you can not enter an empty field try again !\n";
+                std::getline(std::cin, contact[i]);   
+            }
+        }
+    }
 }
-const std::string& Contact::get_first_name() const
+const std::array<std::string, 4>& Contact::get_contact() const
 {
-    return(first_name);
+    return(contact);
 }
-void Contact::set_last_name(const std::string &value)
-{
-    last_name = value;
-}
-const std::string& Contact::get_last_name() const
-{
-    return(last_name);
-}
-void Contact::set_phone_nbr(const std::string &value)
-{
-    phone_number = value;
-}
-const std::string& Contact::get_phone_nbr() const
-{
-    return(phone_number);
-}
-void Contact::set_dark_sec(const std::string &value)
-{
-    darkest_sec = value;
-}
-const std::string& Contact::get_dark_sec() const
-{
-    return(darkest_sec);
-}
-
 int main ()
 {
     Contact c1;
-    std::string first_name;
-    std::string last_name;
-    std::string phone_number;
-    std::string dark_sec;
-    std::cin >>first_name;
-    std::cin >>last_name;
-    std::cin >>phone_number;
-    std::cin.ignore();
-    std::getline(std::cin, dark_sec);
-    c1.set_first_name(first_name);
-    c1.set_last_name(last_name);
-    c1.set_phone_nbr(phone_number);
-    c1.set_dark_sec(dark_sec);
-    std::cout << c1.get_first_name();
-    std::cout << '\n';
-    std::cout << c1.get_last_name();
-    std::cout << '\n';
-    std::cout << c1.get_phone_nbr();
-    std::cout << '\n';
-    std::cout << c1.get_dark_sec();
-    
+    c1.set_contact();
+    const std::array<std::string, 4>& input = c1.get_contact(); //refrences in c++ should always be declared and initialized at the same time
+    for(int i = 0; i < 4; i++)
+    {
+        std::cout << input[i];
+        std::cout << '\n';  
+    }   
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -3,23 +3,15 @@
 
 #include <string>
 #include <iostream>
+#include <array>
 
 class Contact
 {
     private :
-        std::string first_name;
-        std::string last_name;
-        std::string phone_number;
-        std::string darkest_sec;
+        std::array<std::string, 4> contact;
     public :
-        void set_first_name(const std::string& value);
-        const std::string& get_first_name() const;
-        void set_last_name(const std::string& value);
-        const std::string& get_last_name() const;
-        void set_phone_nbr(const std::string& value);
-        const std::string& get_phone_nbr() const;
-        void set_dark_sec(const std::string& value);
-        const std::string& get_dark_sec() const;
+        void set_contact();
+        const std::array<std::string, 4>& get_contact() const;
 };
 
 #endif
