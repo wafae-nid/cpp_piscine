0aeaa9676194c5e464bd7fe2a6e6a0bcbcb19e9d
Wafae Nid hsain <wafaenido8@gmail.com>
2026-01-18 19:40:41 +0100

    my first phonebook need the last element

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: wnid-hsa <wnid-hsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:22:02 by wnid-hsa          #+#    #+#             */
-/*   Updated: 2026/01/18 14:04:18 by wnid-hsa         ###   ########.fr       */
+/*   Updated: 2026/01/18 19:38:21 by wnid-hsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,96 @@ const std::array<std::string, 4>& Contact::get_contact() const
 {
     return(contact);
 }
-int main ()
+int PhoneBook::count = 0;
+
+void PhoneBook::add()
 {
-    Contact c1;
-    c1.set_contact();
-    const std::array<std::string, 4>& input = c1.get_contact(); //refrences in c++ should always be declared and initialized at the same time
+   Contact contact;
+   
+   contact.set_contact();
+   if(count == 3)
+    count = 0;
+   book[count] = contact;
+//    std::cout<< count;
+//    std::cout<<'\n';
+//    std::cout<< book[count].get_contact()[0];
+   count++; 
+}
+void PhoneBook::exiting()
+{
+    std::cout<< "you exited phonebook all contacts are forever lost\n";
+    exit(1);
+}
+void PhoneBook::line_display()
+{
+    std::cout<< '+';
     for(int i = 0; i < 4; i++)
     {
-        std::cout << input[i];
-        std::cout << '\n';  
-    }   
+        std::cout<< std::string(10,'-')<< '+';
+    }
+    std::cout<< '\n';
+    
+}
+void PhoneBook::static_display()
+{
+    line_display();
+    std::cout<< '|'<< std::setw(10) << "index"  ;
+    std::cout<< '|'<< std::setw(10) << "fisrt name" ;
+    std::cout<< '|'<< std::setw(10) << "last name" ;
+    std::cout<< '|'<< std::setw(10) << "nickname" << '|' ;
+    std::cout<< '\n';
+}
+
+void PhoneBook::dynamic_display()
+{
+    std::string str;
+    
+    for(int i = 0; i < count; i++)
+    {
+        line_display();
+        std::cout<< '|'<< std::setw(10) << i;
+        for(int j = 0; j < 3;j++)
+        {
+            str = book[i].get_contact()[j];
+            if(str.length()> 10)
+                std::cout<< '|'<< std::setw(9) << str.substr(0,9)<< '.';
+            else
+                std::cout<< '|'<< std::setw(10) << str ;
+        }
+        std::cout << '|';
+        std::cout<< '\n';
+    }
+    line_display();
+}
+void PhoneBook::search()
+{
+    static_display();
+    dynamic_display();
+}
+PhoneBook::PhoneBook()
+{
+    std::string input;
+    while(1)
+    {
+        if(!std::getline(std::cin, input))
+        {
+            std::cout<<"you successfully exited the PhoneBook\n";
+            exit(0);
+        }
+        if(input.compare("ADD") == 0)
+            add();
+        else if(input.compare("SEARCH") == 0)
+            // std::cout<<"search\n";
+           search();
+        else if(input.compare("EXIT") == 0)
+            exiting();
+        
+    }
+}
+
+int main ()
+{
+   
+    PhoneBook phonebook;
+    return(0);   
 }
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <array>
+#include <iomanip>
 
 class Contact
 {
@@ -13,5 +14,18 @@ class Contact
         void set_contact();
         const std::array<std::string, 4>& get_contact() const;
 };
-
+class PhoneBook
+{
+    private :
+        Contact book[8];
+        static int     count;
+        void    add();
+        void    static_display();
+        void    line_display();
+        void    dynamic_display();
+        void    search();
+        void    exiting();
+    public:
+       PhoneBook();  
+};
 #endif
