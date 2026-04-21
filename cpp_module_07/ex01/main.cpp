c3416df85b0f6da4c359322236e1d89ff539fa43
Wafae Nid hsain <wafaenido8@gmail.com>
2026-04-21 23:52:24 +0100

    cleaner version use of iter function only

--- cpp_module_07/ex01/main.cpp
+++ cpp_module_07/ex01/main.cpp
@@ -6,13 +6,9 @@ int main( void ) {
 
     int arr[] = {0,1, 2, 3,4};
     size_t size = sizeof(arr) / sizeof(arr[0]);
-    for(size_t i = 0; i < size ; i++)
-    {
-        print_array(arr[i]); 
-    }
+
+    iter(arr, size, print_array<int>);
     iter(arr, size, function<int>);
-    for(size_t i = 0; i < size ; i++)
-    {
-        print_array(arr[i]); 
-    }
+    iter(arr, size, print_array<int>);
+   
 }
