8ff5658194e7c1fe5c9a0fc591809da9c27d3d3a
Wafae Nid hsain <wafaenido8@gmail.com>
2026-05-10 17:58:43 +0100

    added c++98

--- cpp_module_06/ex01/Serializer.hpp
+++ cpp_module_06/ex01/Serializer.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <iostream>
 #include <cctype>
-
+#include <stdint.h>
 #include "Data.hpp"
 
 class Serializer
--- cpp_module_06/ex02/Base.hpp
+++ cpp_module_06/ex02/Base.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <iostream>
 #include <ctime>
-
+#include <cstdlib>
 
 class Base
 {
