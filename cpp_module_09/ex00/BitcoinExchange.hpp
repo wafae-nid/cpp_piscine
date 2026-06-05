174f8a6869d401daec7c5187db5c7934fcd8631e
wafae <wafae@DESKTOP-RRB6IHE.localdomain>
2026-06-05 03:53:54 +0100

    starting to work c++ style

--- cpp_module_09/ex00/BitcoinExchange.hpp
+++ cpp_module_09/ex00/BitcoinExchange.hpp
@@ -8,4 +8,20 @@
 #include <map>
 #include <iomanip>
 
+std::map<std::string,float> data_to_map(void);
+
+class BitcoinExchange
+{
+    private:
+        std::map<std::string,float> map_db;
+    public:
+        BitcoinExchange();
+    // BitcoinExchange(const BitcoinExchange& copy);
+    // BitcoinExchange& operator=(const BitcoinExchange& copy);
+
+        ~BitcoinExchange();
+    
+    
+};
+
 #endif
