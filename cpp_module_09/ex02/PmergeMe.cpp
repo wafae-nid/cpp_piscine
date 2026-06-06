be82d90fbaf40bdac9b95daf1a3b7c6807cc0abf
wafae <wafae@DESKTOP-RRB6IHE.localdomain>
2026-06-06 23:58:12 +0100

    make pairs made

--- cpp_module_09/ex02/PmergeMe.cpp
+++ cpp_module_09/ex02/PmergeMe.cpp
@@ -1,6 +1,6 @@
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe()
+PmergeMe::PmergeMe():has_leftover(false),leftover(-1)
 {
 
 }
@@ -8,7 +8,88 @@ PmergeMe::~PmergeMe()
 {
 
 }
+template<typename Container>
+void PmergeMe::print(const Container &c)
+{
+     for(size_t i = 0; i < c.size();i++)
+    {
+        std::cout<< c[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
+std::deque<pair> PmergeMe::make_pairs_deq(std::deque<int> d)
+{
+    pair p;
+    std::deque<pair> pairs;
+
+    size_t i = 0;
+    while((i + 1)< d.size())
+    {
+        int a = d[i];
+        int b = d[i+1];
+        if(a > b)
+        {
+            p.big = a;
+            p.small = b;
+        }
+        else
+        {
+            p.big = b;
+            p.small = a;
+        }
+        pairs.push_back(p);
+        i+=2;
+    }
+     if (i < vec.size())
+    {
+        leftover = vec[i];
+        has_leftover = true;
+    }
+    return(pairs);
+   
+}
+std::vector<pair> PmergeMe::make_pairs_vec(std::vector<int> d)
+{
+    pair p;
+    std::vector<pair> pairs;
+
+    size_t i = 0;
+    while((i + 1)< d.size())
+    {
+        int a = d[i];
+        int b = d[i+1];
+        if(a > b)
+        {
+            p.big = a;
+            p.small = b;
+        }
+        else
+        {
+            p.big = b;
+            p.small = a;
+        }
+        pairs.push_back(p);
+        i+=2;
+    }
+     if (i < vec.size())
+    {
+        leftover = vec[i];
+        has_leftover = true;
+    }
+    return(pairs);
+   
+}
+std::deque<int> PmergeMe::fordJohnsonDeque(std::deque<int> d)
+{
+    std::deque<pair> pairs = make_pairs_deq(d);
+    return(d);
+} 
+std::vector<int> PmergeMe::fordJohnsonVector(std::vector<int> v)
+{
+    std::vector<pair> pairs = make_pairs_vec(v);
+    return(v);
+}
 bool PmergeMe::parse_input(std::string str)
 {
     if (str.empty())
@@ -20,23 +101,51 @@ bool PmergeMe::parse_input(std::string str)
     }
     return(true);
 }
- void PmergeMe::run(char **argv, int argc)
- {
+bool PmergeMe::setup(char **argv, int argc)
+{
     for(int i = 1; i < argc; i++)
     {
         if(!parse_input(argv[i]))
         {
             std::cerr << "ERROR \n";
-            return; 
+            return(false); 
         }
         char *end;
         long nbr = strtol(argv[i], &end, 10);
         if(*end !='\0' ||nbr >INT_MAX )
         {
             std::cerr << "ERROR \n";
-            return; 
+            return(false); 
         }
-        vec.push_back(nbr);
-        deq.push_back(nbr);
+        vec.push_back(static_cast<int>(nbr));
+        deq.push_back(static_cast<int>(nbr));
     }
+    return(true);
+}
+ void PmergeMe::run(char **argv, int argc)
+ {
+    if(!setup(argv,argc))
+        return;
+    std::cout << "Before: ";
+    print(vec);
+    clock_t start1 = clock();
+
+    std::vector<int> sorted_vec = fordJohnsonVector(vec);
+
+    clock_t end1 = clock();
+
+    clock_t start2 = clock();
+
+   std::deque<int> sorted_deq = fordJohnsonDeque(deq);
+
+    clock_t end2 = clock();
+
+    //std::cout << "After: ";
+    //print(sorted_vec);
+
+    double time_vec = (double)(end1 - start1)/ CLOCKS_PER_SEC;
+    double time_deq = (double)(end2 - start2)/ CLOCKS_PER_SEC;
+
+    std::cout << "Time vector: " << time_vec << " us\n";
+    std::cout << "Time deque : " << time_deq << " us\n";
  }
--- cpp_module_09/ex02/PmergeMe.hpp
+++ cpp_module_09/ex02/PmergeMe.hpp
@@ -7,6 +7,15 @@
 #include <deque>
 #include <stdlib.h>
 #include <limits.h>
+#include <ctime>
+
+struct pair
+{
+    int big ;
+    int small;
+
+};
+
 class PmergeMe
 {
     public:
@@ -14,9 +23,22 @@ class PmergeMe
        ~PmergeMe();
       bool parse_input(std::string str);
       void run(char **argv, int argc);
+
+      
+
+      template<typename Container>
+      void print(const Container &c);
+
+      std::vector<int> fordJohnsonVector(std::vector<int> v);
+      std::deque<int> fordJohnsonDeque(std::deque<int> d);
+      std::deque<pair> make_pairs_deq(std::deque<int> d);
+      std::vector<pair> make_pairs_vec(std::vector<int> d);
+      bool setup(char **argv, int argc);   
     private:
         std::vector<int> vec;
-        std::deque<int> deq;
+        std::deque<int> deq; 
+        bool has_leftover;
+        int leftover;
 
 };
 
